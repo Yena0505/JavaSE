6ce571b98ae4e361ea79a7091a5656c934bc478c
yangyena <yangyena@DESKTOP-GIGH4A4>
2023-07-24 19:12:10 +0900

    second commit

---
+++ src/section02/Variable02.java
@@ -0,0 +1,20 @@
+package section02;
+/*
+ * 2. 문자형(char)
+ * 문자 한 글자 저장 가능 - 전 세계 쓰이는 대부분 문자 표현 가능
+ * 2byte
+ * 
+ * 선언 방법 
+ * char 변수명;
+ * 
+ * 변수 초기화
+ * 변수를 사용하기 전 값을 처음 할당하는 것을 의미한다.
+ */
+public class Variable02 {
+	public static void main(String[] args) {
+		//char 형 변수 선언 및 대입
+		char ch1 = 'A';
+		System.out.println("char 형 변수에 저장된 문자");
+	}
+
+}
